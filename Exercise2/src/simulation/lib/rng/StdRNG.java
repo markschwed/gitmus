b7646c620a05f7d55c13d4f7c9bc5057df68978f
foxxer <mail@markschwed.de>
2018-10-28 13:20:11 +0100

    2.3.1 done

--- Exercise2/src/simulation/lib/rng/StdRNG.java
+++ Exercise2/src/simulation/lib/rng/StdRNG.java
@@ -25,9 +25,17 @@ public class StdRNG extends RNG {
 	@Override
 	public void setSeed(long seed) {
 		/*
-		 * TODO Problem 2.3.1 - setSeed
+		 * Problem 2.3.1 - setSeed
 		 * Update the seed of your random number generator rng (Hint: rng can be null @see RNG)
 		 */
+		
+		//Mark
+		if (rng == null) {
+			rng = new Random(seed);
+		} else {
+			rng.setSeed(seed);
+		}
+		//Mark out
 	}
 
 	/**
@@ -36,10 +44,15 @@ public class StdRNG extends RNG {
 	@Override
 	public double rnd() {
 		/*
-		 * TODO Problem 2.3.1 - rnd
+		 * Problem 2.3.1 - rnd
 		 * Create a random value: 0 < value < 1
 		 */
-		double rv = 0;
-		return rv;
+		double tmp;
+		//Mark
+		do {
+			tmp = rng.nextDouble();
+		} while (tmp == 0);
+		return tmp;
+		//Mark out
 	}
 }
