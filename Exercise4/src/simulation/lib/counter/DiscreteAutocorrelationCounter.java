9b63e221841bca9f2d2500a84272755ccbb6015e
foxxer <mail@markschwed.de>
2018-11-07 00:52:37 +0100

    created structure for 4.1

--- Exercise4/src/simulation/lib/counter/DiscreteAutocorrelationCounter.java
+++ Exercise4/src/simulation/lib/counter/DiscreteAutocorrelationCounter.java
@@ -8,18 +8,68 @@ import java.util.LinkedList;
 /**
  * This class implements a discrete time autocorrelation counter
  */
-public class DiscreteAutocorrelationCounter {
-	/*
-     * TODO Problem 4.1.1 - Implement this class according to the given class diagram!
-     * Hint: see section 4.4 in course syllabus
-     */
+public class DiscreteAutocorrelationCounter extends DiscreteCounter{
+	
+	private int maxLag;
+	private double[] firstValues;
+	private double[] lastValues;
+	private double[] squaredSum;
+	
+	public DiscreteAutocorrelationCounter(String variable, int maxLag) {
+		this(variable, "counter type: discrete autocorrelation counter", maxLag);
+	}
 
+	public DiscreteAutocorrelationCounter(String variable, String type, int maxLag) {
+		super(variable);
+		if(maxLag < 0) {
+			throw new IllegalArgumentException("maxLag needs to be 0 or greater");
+		}
+		setMaxLag(maxLag);
+		reset();
+	}
+	
+	public long getMaxLag() {
+		return maxLag;
+	}
+	
+	public void setMaxLag(long maxLag) {
+		this.maxLag = (int) maxLag;
+		reset();
+	}
+	
+	public void count(double x) {
+		super.count(x);
+		
+		//TODO
+	}
+	
+	public void reset() {
+		super.reset();
+		firstValues = new double[maxLag];
+		lastValues = new double[maxLag];
+		squaredSum = new double[maxLag+1];
+	}
+	
+	public double getAutoCovariance(int lag) {
+		if (lag > maxLag || lag > getNumSamples()) {
+			throw new IllegalArgumentException("your lag is greater than maxLag or greater than the number of samples");
+		}
+		
+		//TODO
+		return 1;
+	}
 
-	/**
-	 * @see Counter#report()
-	 * TODO Uncomment this function if you have implemented the class!
-	 */
-	/*@Override
+	public double getAutoCorrelation(int lag) {
+		if (lag > maxLag) {
+			throw new IllegalArgumentException("your lag is greater than maxLag");
+		}
+		if (getVariance() == 0) {
+			return 1;
+		}
+		return getAutoCovariance(lag) / getVariance();
+	}
+	
+	@Override
 	public String report() {
 		String out  = super.report();
 		out += ("\n\tCorrelation/Covariance:\n");
@@ -29,12 +79,9 @@ public class DiscreteAutocorrelationCounter {
 					"correlation = " + getAutoCorrelation(i)+"\n");
 		}
 		return out;
-	}*/
-	/**
-	 * @see Counter#csvReport(String)
-	 * TODO Uncomment this function if you have implemented the class!
-	 */
-	/*@Override
+	}
+	
+	@Override
 	public void csvReport(String outputdir){
 	    String content = "";
         for(int i = 0; i <= (getNumSamples() < maxLag ? getNumSamples() : maxLag); i++) {
@@ -44,5 +91,5 @@ public class DiscreteAutocorrelationCounter {
         }
         String labels = "#counter ; numSamples ; MEAN; VAR; STD; CVAR; MIN; MAX; COV; CORR\n";
         writeCsv(outputdir, content, labels);
-	}*/
+	}
 }
